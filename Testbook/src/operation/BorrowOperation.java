19d3a3d3374d202358e7ac3e594578c801ff5e7a
fcawyxb <2377423295@qq.com>
2024-04-21 16:34:56 +0800

    图书管理系统

--- Testbook/src/operation/BorrowOperation.java
+++ Testbook/src/operation/BorrowOperation.java
@@ -1,6 +1,9 @@
 package operation;
 
+import book.Book;
 import book.BookList;
+
+import java.util.Scanner;
 
 /**
  * @ProjectName: Testbook
@@ -15,5 +18,21 @@ public class BorrowOperation implements IOPeration{
     @Override
     public void work(BookList bookList) {
         System.out.println("借阅图书");
+        //1.你要借哪本书
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("输入你想要借的书名");
+        String name = scanner.nextLine();
+        int currentSize = bookList.getUsedSize();
+        for (int i = 0; i < currentSize; i++) {
+            Book book = bookList.getBook(i);
+            if(book.getName().equals(name)){
+                book.setBorrowed(true);
+                System.out.println("借阅成功");
+                System.out.println(book);
+                return;
+            }
+        }
+        System.out.println("你借阅的图书不存在");
     }
+
 }
--- Testbook/src/operation/ReturnOperation.java
+++ Testbook/src/operation/ReturnOperation.java
@@ -1,6 +1,9 @@
 package operation;
 
+import book.Book;
 import book.BookList;
+
+import java.util.Scanner;
 
 /**
  * @ProjectName: Testbook
@@ -16,5 +19,21 @@ public class ReturnOperation implements IOPeration{
     @Override
     public void work(BookList bookList) {
         System.out.println("归还图书");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("输入你想要归还的书名");
+        String name = scanner.nextLine();
+        int currentSize = bookList.getUsedSize();
+        for (int i = 0; i < currentSize; i++) {
+            Book book = bookList.getBook(i);
+            if(book.getName().equals(name)){
+                book.setBorrowed(false);
+                System.out.println("归还成功");
+                System.out.println(book);
+                return;
+            }
+        }
+        System.out.println("你归还的图书不存在");
     }
+
+
 }
