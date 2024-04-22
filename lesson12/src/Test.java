4765507555cf7f417599ad9e5486331f439e09ef
fcawyxb <2377423295@qq.com>
2024-04-22 22:54:15 +0800

    异常

---
+++ lesson12/src/Test.java
@@ -0,0 +1,17 @@
+/**
+ * @ProjectName: lesson12
+ * @Package: PACKAGE_NAME
+ * @ClassName: Test
+ * @author: Yxb
+ * @Description:
+ * @Date: 2024/4/21 16:40
+ * @Version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a = 10;
+        if(a == 10){
+            throw new NullPointerException("ahhahahahahha");
+        }
+    }
+}
