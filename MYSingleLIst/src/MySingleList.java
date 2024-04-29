751a3d67548a71459b6c849edb70c4d071d0d593
fcawyxb <2377423295@qq.com>
2024-04-29 20:10:41 +0800

    单链表

---
+++ MYSingleLIst/src/MySingleList.java
@@ -0,0 +1,19 @@
+/**
+ * @ProjectName: MYSingleLIst
+ * @Package: PACKAGE_NAME
+ * @ClassName: MySingleList
+ * @author: Yxb
+ * @Description:
+ * @Date: 2024/4/29 17:56
+ * @Version: 1.0
+ */
+public class MySingleList implements IinkList{
+        static class ListNode{
+        public int val;
+        public ListNode node;
+
+        public ListNode(int val){
+            this.val = val;
+        }
+    }
+}
---
+++ MYSingleLIst/src/Text.java
@@ -0,0 +1,14 @@
+/**
+ * @ProjectName: MYSingleLIst
+ * @Package: PACKAGE_NAME
+ * @ClassName: Text
+ * @author: Yxb
+ * @Description:
+ * @Date: 2024/4/29 17:57
+ * @Version: 1.0
+ */
+public class Text {
+    public static void main(String[] args) {
+
+    }
+}
