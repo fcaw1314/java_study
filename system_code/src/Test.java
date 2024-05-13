bd2aa6ad282ae0c1a7976d135d8d9bdda9fd3773
fcawyxb <2377423295@qq.com>
2024-05-13 23:34:48 +0800

    Thread

--- system_code/src/Test.java
+++ system_code/src/Test.java
@@ -1,11 +1,16 @@
-package PACKAGE_NAME;/**
- * @ProjectName:    system_code 
- * @Package:        PACKAGE_NAME
- * @ClassName:      Test
- * @author:     Yxb
- * @Description:    
- * @Date:    2024/5/12 16:09
- * @Version:    1.0
- */ 
+/**
+ * @ProjectName: system_code
+ * @Package: PACKAGE_NAME
+ * @ClassName: Test
+ * @author: Yxb
+ * @Description:
+ * @Date: 2024/5/12 16:09
+ * @Version: 1.0
+ */
+
+
 public class Test {
+    public static void main(String[] args) {
+
+    }
 }
--- system_code/src/thread/Demo1.java
+++ system_code/src/thread/Demo1.java
@@ -1,11 +1,44 @@
-package thread;/**
- * @ProjectName:    system_code 
- * @Package:        thread
- * @ClassName:      Demo1
- * @author:     Yxb
- * @Description:    
- * @Date:    2024/5/12 16:09
- * @Version:    1.0
- */ 
+package thread;
+
+import java.util.jar.JarOutputStream;
+
+/**
+ * @ProjectName: system_code
+ * @Package: thread
+ * @ClassName: Demo1
+ * @author: Yxb
+ * @Description:
+ * @Date: 2024/5/12 16:09
+ * @Version: 1.0
+ */
+class MyThread extends Thread{
+    @Override
+    public void run() {
+        //System.out.println("Hello Thread!!");
+        while(true){
+            System.out.println("Hello Thread!!");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
 public class Demo1 {
+    public static void main(String[] args) {
+        Thread t = new MyThread();
+        t.start();
+        while(true){
+            System.out.println("Hello main!!!");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+    }
 }
