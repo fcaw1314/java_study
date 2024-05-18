27b98c2ed7f611c124d2c4027aeede04df691420
fcawyxb <2377423295@qq.com>
2024-05-18 23:15:54 +0800

    join等待一个线程

---
+++ Hash/src/HashBuck2.java
@@ -0,0 +1,11 @@
+package PACKAGE_NAME;/**
+ * @ProjectName:    Hash 
+ * @Package:        PACKAGE_NAME
+ * @ClassName:      HashBuck2
+ * @author:     Yxb
+ * @Description:    
+ * @Date:    2024/5/17 7:58
+ * @Version:    1.0
+ */ 
+public class HashBuck2 {
+}
---
+++ system_code/src/thread/Demo5.java
@@ -0,0 +1,32 @@
+package thread;
+
+/**
+ * @ProjectName: system_code
+ * @Package: thread
+ * @ClassName: Demo5
+ * @author: Yxb
+ * @Description:
+ * @Date: 2024/5/18 21:23
+ * @Version: 1.0
+ */
+public class Demo5 {
+    public static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 50000; i++) {
+                count++;
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 50000; i++) {
+                count++;
+            }
+        });
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+        System.out.println("count = " + count);
+    }
+}
