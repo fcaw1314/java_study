a7cdb412d0d141033e8eb0c747cd19a221c8be69
fcawyxb <2377423295@qq.com>
2024-05-20 19:39:33 +0800

    homework

--- system_code/src/thread/Demo6.java
+++ system_code/src/thread/Demo6.java
@@ -1,11 +1,44 @@
-package thread;/**
- * @ProjectName:    system_code 
- * @Package:        thread
- * @ClassName:      Demo6
- * @author:     Yxb
- * @Description:    
- * @Date:    2024/5/19 8:51
- * @Version:    1.0
- */ 
+package thread;
+
+import java.util.Random;
+
+/**
+ * @ProjectName: system_code
+ * @Package: thread
+ * @ClassName: Demo6
+ * @author: Yxb
+ * @Description:
+ * @Date: 2024/5/19 8:51
+ * @Version: 1.0
+ */
 public class Demo6 {
+    public static void main(String[] args) throws InterruptedException {
+        int[] arr = new int[10000000];
+        Random random = new Random();
+        for(int i = 0 ; i<arr.length;i++){
+            arr[i] =random.nextInt(100);
+        }
+        //第一个线程，返回偶数之和。
+        Thread t1 = new Thread(()->{
+            int temp = 0;
+            for (int i = 0; i < arr.length; i+=2) {
+                temp += arr[i];
+            }
+            System.out.println("线程1：" + temp);
+        });
+        //第二个线程，放回奇数之和。
+        Thread t2 = new Thread(()->{
+            int temp = 0;
+            for (int i = 1; i < arr.length; i+=2) {
+                temp += arr[i];
+            }
+            System.out.println("线程1：" + temp);
+        });
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+
+       // System.out.println("总和"+sum);//打印所有奇数偶数下标的和
+    }
 }
--- system_code/src/thread/Demo7.java
+++ system_code/src/thread/Demo7.java
@@ -1,11 +1,25 @@
-package thread;/**
- * @ProjectName:    system_code 
- * @Package:        thread
- * @ClassName:      Demo7
- * @author:     Yxb
- * @Description:    
- * @Date:    2024/5/19 9:41
- * @Version:    1.0
- */ 
+package thread;
+
+/**
+ * @ProjectName: system_code
+ * @Package: thread
+ * @ClassName: Demo7
+ * @author: Yxb
+ * @Description:
+ * @Date: 2024/5/19 9:41
+ * @Version: 1.0
+ */
 public class Demo7 {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 20; i++) {
+            int tmp = i;
+            Thread t = new Thread(()->{
+                System.out.println(tmp);
+            });
+            t.start();
+            t.join();
+        }
+
+        System.out.println("ok");
+    }
 }
