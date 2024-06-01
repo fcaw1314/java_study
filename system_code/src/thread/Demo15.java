bb22025894dcf7881729c7458c3a2ffe67b0d552
fcawyxb <2377423295@qq.com>
2024-06-01 20:35:54 +0800

    懒汉模式的线程安全创建方法

--- system_code/src/thread/Demo15.java
+++ system_code/src/thread/Demo15.java
@@ -10,22 +10,24 @@ package thread;
  * @Version: 1.0
  */
 class SingletonLazy{
-    private static volatile SingletonLazy instance = null;
+    // 声明，并用volatile修饰，保证在多线程环境下的有序性
+    private volatile static SingletonLazy instance = null;
     private static Object locker = new Object();
-
-    public static SingletonLazy getInstance() {
+    // 私有构造方法
+    private SingletonLazy () {}
+    // 对外提供一个获取实例的方法，
+    private static SingletonLazy getInstance() {
+        // 使用双重if检查, 降低锁竞争的频率
         if (instance == null) {
+            // instance没有被实例化才去加锁
             synchronized (locker) {
+                // 获取到锁后再检查一遍，确认instance没有被实例化后去真正的实例化
                 if (instance == null) {
                     instance = new SingletonLazy();
                 }
             }
         }
         return instance;
-    }
-    private SingletonLazy(){
-
-
     }
 }
 
