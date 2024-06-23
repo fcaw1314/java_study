981f0d5fb64a8e2ac31560258b79613b2b840779
派大星 <2377423295@qq.com>
2024-06-23 10:08:17 +0800

    阻塞队列

--- system_code/src/thread/Demo30.java
+++ system_code/src/thread/Demo30.java
@@ -1,5 +1,9 @@
 package thread;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * @author yxb
  * @version 1.0
@@ -8,6 +12,37 @@ package thread;
  */
 public class Demo30 {
     public static void main(String[] args) {
+        BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(1000);
 
+        //消费者
+        Thread t1 = new Thread(() -> {
+            try {
+                while (true) {
+                    Integer value = queue.take();
+                    System.out.println("t1 消费: " + value);
+                    //每隔1s生产一个元素
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        //生产者
+        Thread t2 = new Thread(() -> {
+            try {
+                int count = 1;
+                while (true) {
+                    queue.put(count);
+                    System.out.println("t2 生产了: " + count);
+                    count++;
+
+                }
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        t1.start();
+        t2.start();
     }
 }
---
+++ system_code/src/thread/Demo31.java
@@ -0,0 +1,92 @@
+package thread;
+
+/**
+ * @author yxb
+ * @version 1.0
+ * @description:
+ * @date 2024/6/22 9:12
+ */
+
+class MyBlockingQueue {
+    private String[] elems = null;
+    //[head,tail)
+    //head 指的是第一个元素,tail指的是最后一个元素的下一个原始
+    private int head = 0;
+    private int tail = 0;
+    private int size = 0;
+
+    public MyBlockingQueue(int capacity) {
+        elems = new String[capacity];
+    }
+
+    void put(String elem) throws InterruptedException {
+        synchronized (this){
+            while(size >= elems.length) {
+                //队列满了
+                this.wait();
+            }
+            elems[tail] = elem;
+            tail++;
+            if(tail >= elems.length) {
+                //到达末尾,就回到开头
+                tail = 0;
+            }
+            //更新size值
+            size++;
+            //唤醒下面take阻塞的wait操作
+            this.notify();
+        }
+    }
+//出队列
+    String take() throws InterruptedException {
+        synchronized (this) {
+            while(size == 0) {
+                this.wait();
+            }
+            //取出head指向的元素
+            String result = elems[head];
+            head++;
+            if(head >= elems.length) {
+                head = 0;
+            }
+            size--;
+
+            //take成功一个元素,就唤醒上面的put中的wait操作
+            this.notify();
+            return result;
+        }
+    }
+}
+
+public class Demo31 {
+    public static void main(String[] args) {
+        MyBlockingQueue queue = new MyBlockingQueue(1000);
+
+        Thread t1 = new Thread(() -> {
+            try {
+                int count = 1;
+                while (true) {
+                    queue.put(count + "");
+                    System.out.println("生产" + count);
+                    count++;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                while (true) {
+                    String result = queue.take();
+                    System.out.println("消费" + result);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+}
