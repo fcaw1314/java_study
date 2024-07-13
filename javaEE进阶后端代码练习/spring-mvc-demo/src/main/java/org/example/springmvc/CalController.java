3a6f057faaed20583ee362a93ec655c6f1af415e
fcawyxb <2377423295@qq.com>
2024-07-14 00:12:55 +0800

    feat:计算机后端页面

---
+++ javaEE进阶后端代码练习/spring-mvc-demo/src/main/java/org/example/springmvc/CalController.java
@@ -0,0 +1,22 @@
+package org.example.springmvc;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yxb
+ * @version 1.0
+ * @description:
+ * @date 2024/7/13 9:59
+ */
+
+@RestController
+@RequestMapping("/calc")
+public class CalController {
+    @RequestMapping("/sum")
+    public String sum(Integer num1, Integer num2) {
+        //参数校验省略
+        Integer sum = num1 + num2;
+        return "计算结果: " + sum;
+    }
+}
