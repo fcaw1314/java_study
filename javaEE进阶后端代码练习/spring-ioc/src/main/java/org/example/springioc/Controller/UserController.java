831642138c9ed4ef71f20d096190a45ed11cb449
fcawyxb <2377423295@qq.com>
2024-07-18 10:20:05 +0800

    update

---
+++ javaEE进阶后端代码练习/spring-ioc/src/main/java/org/example/springioc/Controller/UserController.java
@@ -0,0 +1,17 @@
+package org.example.springioc.Controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author yxb
+ * @version 1.0
+ * @description:
+ * @date 2024/7/17 14:56
+ */
+@Controller
+public class UserController {
+    public void sayHi(){
+        System.out.println("hi, UserController....");
+    }
+
+}
