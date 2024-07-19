cb8fec8f5298a489123b4bcc7286011262778f00
fcawyxb <2377423295@qq.com>
2024-07-19 22:30:53 +0800

    自动化测试

---
+++ 测试/seleniumAutoTest/seleniumAutoTest/src/test/java/runCase.java
@@ -0,0 +1,12 @@
+/**
+ * @author yxb
+ * @version 1.0
+ * @description:
+ * @date 2024/7/18 16:44
+ */
+public class runCase {
+    public static void main(String[] args) throws InterruptedException {
+        FirstTest test = new FirstTest();
+        test.test01();
+    }   
+}
