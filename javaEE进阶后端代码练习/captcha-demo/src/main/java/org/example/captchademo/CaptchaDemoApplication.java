454ba3a312cf3e207b724d3fdcba9302d407e9ab
fcawyxb <2377423295@qq.com>
2024-07-21 11:30:58 +0800

    实现一个简单的验证码

---
+++ javaEE进阶后端代码练习/captcha-demo/src/main/java/org/example/captchademo/CaptchaDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.captchademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptchaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaptchaDemoApplication.class, args);
+    }
+
+}
