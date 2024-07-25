ed3579caa96b693f21a806a5d513dcd953d5e775
不喝饮料 <2377423295@qq.com>
2024-07-25 21:23:55 +0800

    mybatis练习up

--- javaEE进阶后端代码练习/spring-mvc-demo/src/main/java/org/example/springmvc/coontroller/CalController.java
+++ javaEE进阶后端代码练习/spring-mvc-demo/src/main/java/org/example/springmvc/coontroller/CalController.java
@@ -1,4 +1,4 @@
-package org.example.springmvc;
+package org.example.springmvc.coontroller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
