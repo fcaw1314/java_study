d7c12e62c458e30545fba2a8f6127a94227ff4f0
不喝饮料 <2377423295@qq.com>
2024-07-25 21:25:27 +0800

    mybatis练习up

--- javaEE进阶后端代码练习/spring-mvc-demo/src/main/java/org/example/springmvc/coontroller/ResponseController.java
+++ javaEE进阶后端代码练习/spring-mvc-demo/src/main/java/org/example/springmvc/coontroller/ResponseController.java
@@ -1,9 +1,8 @@
-package org.example.springmvc;
+package org.example.springmvc.coontroller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author yxb
