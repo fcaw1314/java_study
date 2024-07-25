daa7db94c2aeef779d1e1961be23387111d94606
不喝饮料 <2377423295@qq.com>
2024-07-25 21:24:58 +0800

    mybatis练习up

--- javaEE进阶后端代码练习/spring-mvc-demo/src/main/java/org/example/springmvc/coontroller/RequestController.java
+++ javaEE进阶后端代码练习/spring-mvc-demo/src/main/java/org/example/springmvc/coontroller/RequestController.java
@@ -1,8 +1,7 @@
-package org.example.springmvc;
+package org.example.springmvc.coontroller;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
